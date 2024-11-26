c6a01eb48635e201fdb8b7606002b9a0b4b9e548
HasnainAli313 <aliorakzai407@gmail.com>
2024-11-26 16:52:44 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// script.js
+document.addEventListener('DOMContentLoaded', function () {
+    const menuToggle = document.getElementById('menu-toggle');
+    const mobileNav = document.getElementById('mobile-nav');
+
+    menuToggle.addEventListener('click', function () {
+        mobileNav.classList.toggle('open');
+    });
+});
