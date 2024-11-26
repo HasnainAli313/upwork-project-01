722b0647c476f2008e9ccfb08f122e492b943347
HasnainAli313 <aliorakzai407@gmail.com>
2024-11-26 23:12:19 +0500

    done

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-// script.js
 document.addEventListener('DOMContentLoaded', function () {
     const menuToggle = document.getElementById('menu-toggle');
     const mobileNav = document.getElementById('mobile-nav');
+    const navLinks = mobileNav.querySelectorAll('a');
 
     menuToggle.addEventListener('click', function () {
         mobileNav.classList.toggle('open');
     });
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', function () {
+            mobileNav.classList.remove('open');
+        });
+    });
 });
+
+
+
+let currentDate = new Date();
+let currentYear =currentDate.getFullYear();
+
+let footerText = document.getElementById('footerText');    
+footerText.innerHTML=`Captain Appliance | PO Box 252 Germantown, OH 45327 | © CAPTAIN APPLIANCE LLC ${currentYear} `
+// let currentYear = currentDate.getFullYear();
